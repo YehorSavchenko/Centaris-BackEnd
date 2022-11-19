f5200ab4f052bdc66888d6ca9f7a17495d79dd6f
mateusz <mateusz.soltys@protonmail.com>
2022-11-19 18:36:30 +0100

    Added all get items endpoints

--- src/main/java/backend/controllers/ItemsController.java
+++ src/main/java/backend/controllers/ItemsController.java
@@ -5,6 +5,7 @@ import model.ItemList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,6 +19,28 @@ public class ItemsController {
 
     @GetMapping("/items/get/all")
     public ItemList getAllItems() {
+        ItemList allItems = dataBaseClient.getAllItems();
+        LOGGER.info("Found following items: {}", allItems);
+        return allItems;
+    }
+
+    @GetMapping("/items/get/district/{district}")
+    public ItemList getAllItemsForDistrict(@PathVariable String district) {
+        ItemList allItems = dataBaseClient.getAllItems();
+        LOGGER.info("Found following items: {}", allItems);
+        return allItems;
+    }
+
+    @GetMapping("/items/get/category/{category}")
+    public ItemList getAllItemsForCategory(@PathVariable String category) {
+        ItemList allItems = dataBaseClient.getAllItems();
+        LOGGER.info("Found following items: {}", allItems);
+        return allItems;
+    }
+
+    @GetMapping("/items/get/categoryanddistrict/{category}&{district}")
+    public ItemList getAllItemsForDistrictAndCategory(@PathVariable String category,
+                                                      @PathVariable String district) {
         ItemList allItems = dataBaseClient.getAllItems();
         LOGGER.info("Found following items: {}", allItems);
         return allItems;
